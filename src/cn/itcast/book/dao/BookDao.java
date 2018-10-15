a0b85bf285e5ad6097b4596710e13d7fef0db10e
wuchangming <708841315@qq.com>
2018-10-15 11:34:41 +0800

    可行

--- src/cn/itcast/book/dao/BookDao.java
+++ src/cn/itcast/book/dao/BookDao.java
@@ -6,7 +6,9 @@ import cn.itcast.pager.Expression;
 import cn.itcast.pager.PageBean;
 import cn.itcast.pager.PageConstants;
 import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.ScalarHandler;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,20 +18,22 @@ import java.util.List;
 public class BookDao {
     private QueryRunner qr = new TxQueryRunner();
 
-    private PageBean<Book> findByCriteria(List<Expression> expressionList,int pageCode){
+    private PageBean<Book> findByCriteria(List<Expression> expressionList,int pageCode) throws SQLException {
 
         int pageSize = PageConstants.BOOK_PAGE_SIZE;
-        List<Object> params = new ArrayList<>();
-        StringBuffer whereSql = new StringBuffer("where 1=1");
+        List<String> params = new ArrayList<>();
+        StringBuffer whereSql = new StringBuffer(" where 1=1 ");
         for (Expression expression : expressionList) {
             if (!expression.getOperator().equals("is null")){
-                whereSql.append(" and ").append(expression.getName()).append("= ?");
+                whereSql.append(" and ").append(expression.getName()).append(" "+expression.getOperator()+" ?");
                 params.add(expression.getValue());
 
 
             }
 
         }
+        String sql = "select count(*) from t_book"+whereSql;
+        Number number = (Number) qr.query(sql,new ScalarHandler(),params.toArray());
         System.out.println(whereSql);
         System.out.println(params);
         return null;
@@ -38,11 +42,17 @@ public class BookDao {
 
     public static void main(String[] args) {
         List<Expression> expressions = new ArrayList<>();
-        expressions.add(new Expression("bname","=","java"));
+        expressions.add(new Expression("bname","like","%Java%"));
         expressions.add(new Expression("bid","is null",""));
-        expressions.add(new Expression("image_w","=","....."));
+//        expressions.add(new Expression("image_w","=","....."));
         BookDao dao = new BookDao();
-        dao.findByCriteria(expressions,10);
+
+        try {
+
+            System.out.println(dao.findByCriteria(expressions,10));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
 
     }
